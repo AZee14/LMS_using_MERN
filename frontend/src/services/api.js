ae89aa656d04b85e4f0c6ef63494e41ae01e9f3f
AZee14 <zali19oct@gmail.com>
2026-05-30 14:28:46 +0500

    Update API base URL to include '/api' path

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://lms-using-mern-2avg.onrender.com';
+const API_BASE_URL = 'https://lms-using-mern-2avg.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
